3988f1827eb87d186ef3a0fb41e7a5d1022758d6
Bhautik <bhirpara600@gmail.com>
2025-09-15 16:08:06 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/Appwrite_Testing",
   server: {
     host: "0.0.0.0",
   },
